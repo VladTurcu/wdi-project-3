79cd23844496062b80f383e5c66d1eed328e3b7a
VladTurcu <turcuvlad87@gmail.com>
2017-09-29 10:57:45 +0100

    Creating user model/controller and user show route

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const places = require('../controllers/places');
+const users = require('../controllers/users');
 
 router.route('/places')
   .get(places.index);
+
+router.route('/users')
+  .get(users.show);
 
 module.exports = router;
--- db/seeds.js
+++ db/seeds.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 const { dbURI } = require('../config/environment');
 const Place = require('../models/place');
+const User = require('../models/user');
 
 mongoose.connect(dbURI, { useMongoClient: true });
 
 Place.collection.drop();
+User.collection.drop();
 
 const placeData = [{
   name: 'Two Brothers Bar',
@@ -22,5 +24,19 @@ const placeData = [{
 Place
   .create(placeData)
   .then(places => console.log(`${places.length} places created!`))
+  .catch(err => console.log(err))
+  .finally(() => mongoose.connection.close());
+
+const userdata = [{
+  username: 'Vlad',
+  email: 'vlad@turcu.me',
+  password: '1234567890abcd',
+  countries: ['Grece'],
+  image: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAASSAAAAJDg1ZjlhYTIzLTQ4MmUtNDE1Yi05MWIzLTk2ZGZkYjFjODI0Mg.jpg',
+  coverPicture: 'http://www.matthewwilliams-ellis.com/wp-content/uploads/2016/10/Romania-Transylvania-Travel-and-Landscape-Photography-Workshop-Holiday-Summer-2018-_011.jpg'
+}];
+User
+  .create(userdata)
+  .then((users) => console.log(`${users.length} users created!`))
   .catch(err => console.log(err))
   .finally(() => mongoose.connection.close());
