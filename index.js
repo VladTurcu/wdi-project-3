234aee49c8e6becbbfaa463faf4a366a0ae0a979
VladTurcu <turcuvlad87@gmail.com>
2017-09-29 13:46:48 +0100

    fixind body parser

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express     = require('express');
-const app         = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-mongoose.Promise = require('bluebird');
-const morgan = require('morgan');
-const { dbURI, port }    = require('./config/environment');
-const routes      = require('./config/routes');
+const express           = require('express');
+const app               = express();
+const mongoose          = require('mongoose');
+const bodyParser        = require('body-parser');
+mongoose.Promise        = require('bluebird');
+const morgan            = require('morgan');
+const { dbURI, port }   = require('./config/environment');
+const routes            = require('./config/routes');
 
 mongoose.connect(dbURI);
 
 app.use(morgan('dev'));
-app.use(bodyParser.json());
 
 app.use(express.static(`${__dirname}/public`));
 app.use(bodyParser.json());
