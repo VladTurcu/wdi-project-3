c38685b2df536c4c18d9713c7deda9a2860bbe70
VladTurcu <turcuvlad87@gmail.com>
2017-10-02 14:45:01 +0100

    Stories and places merged and display on homepage

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -1,6 +1,7 @@
 angular
   .module('bemoApp')
-  .controller('MainCtrl', MainCtrl);
+  .controller('MainCtrl', MainCtrl)
+  .controller('MainIndexCtrl', MainIndexCtrl);
 
 MainCtrl.$inject = ['$state', '$auth', 'User'];
 function MainCtrl($state, $auth, User) {
@@ -12,10 +13,60 @@ function MainCtrl($state, $auth, User) {
   if(userId) vm.user = User.get({ id: userId });
   vm.isAuthenticated = $auth.isAuthenticated;
 
-  
+
   function registerShow() {
     vm.registerHidden = !vm.registerHidden;
     console.log(vm.registerHidden);
   }
 
+}
+
+
+
+
+MainIndexCtrl.$inject = ['$state', 'Place', 'Story'];
+function MainIndexCtrl($state, Place, Story) {
+  const vm = this;
+
+
+  placesPush();
+  function placesPush() {
+    return Place
+      .query()
+      .$promise
+      .then(places => {
+        Story
+          .query()
+          .$promise
+          .then(stories => {
+            vm.all = stories.concat(places);
+
+            console.log(vm.all);
+          });
+      });
+  }
+
+  //
+  // mix(vm.all);
+  //
+  // function mix(alli) {
+  //   var m = alli.length, t, i;
+  //
+  //   // While there remain elements to shuffle
+  //   while (m) {
+  //     // Pick a remaining element…
+  //     i = Math.floor(Math.random() * m--);
+  //
+  //     // And swap it with the current element.
+  //     t = alli[m];
+  //     alli[m] = alli[i];
+  //     alli[i] = t;
+  //   }
+  //
+  //   return alli;
+  // }
+  //
+
+
+  // console.log(vm.all);
 }
