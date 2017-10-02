768371bac1c5af22944cd2c148ddc7cf86c5a981
jdLYNC <joshlynch@live.co.uk>
2017-10-02 10:48:12 +0100

    sliding register form working

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -5,11 +5,16 @@ angular
 MainCtrl.$inject = ['$state', '$auth', 'User'];
 function MainCtrl($state, $auth, User) {
   const vm = this;
+  vm.registerHidden = true;
+  vm.registerShow = registerShow;
   // vm.user = {};
   const { userId } = $auth.getPayload();
 
   if(userId) vm.user = User.get({ id: userId });
-  console.log(typeof(vm.user));
 
+  function registerShow() {
+    vm.registerHidden = !vm.registerHidden;
+    console.log(vm.registerHidden);
+  }
 
 }
